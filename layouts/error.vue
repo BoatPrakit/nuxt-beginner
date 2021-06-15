2c36012fe2d88d35f80857f3600bf042672233d1
Prakit Wongnuntananon <boatnakrub_@hotmail.com>
2021-06-15 23:26:47 +0700

    [Update] nuxt content

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        this is error page
+        <h1>error status: {{ error.statusCode }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['error'],
+    layout: 'error-layout'
+}
+</script>
---
+++ pages/about/_id.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Your param id: {{ id }}</h1>
+</template>
+
+<script>
+export default {
+  validate({ params }) {
+    return params.id === '1';
+  },
+  data() {
+    return {
+      id: this.$route.params.id,
+    };
+  },
+};
+</script>
+
+<style></style>
